f8f6a34d971f2d6f2636b304687f3df9f67a93fa
Sebastian Cuervo <jsebascuervo05@gmail.com>
2024-09-28 23:23:06 -0500

    Inicios del login

---
+++ pages/page.login.php
@@ -0,0 +1,18 @@
+<link rel="stylesheet" href="./assets/css/login.css">
+
+<div class="main">
+    <div class="section-1">Seccion 1</div>
+    <div class="section-2">
+        <div class="content-login">
+            <div class="content-title">Login</div>
+            <div class="content-form">
+                <div class="content-inputs">
+                    <div class="input"></div>
+                    <div class="input"></div>
+                </div>
+                <div class="button"></div>
+            </div>
+            <div class="forgot"></div>
+        </div>
+    </div>
+</div>
