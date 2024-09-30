189871ba36a6015617d23866e3dba3e777ae97e7
Phamela <phamelafuentess@gmail.com>
2024-09-29 23:31:04 -0500

    Avance 1 header

--- components/Header/Header.php
+++ components/Header/Header.php
@@ -1,2 +1,18 @@
 <div class="header">
+    <div class="content-profile">
+        <div class="content-icon-1">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class ="icon-lock">
+            <path stroke-linecap="round" stroke-linejoin="round" d="M16.5 10.5V6.75a4.5 4.5 0 1 0-9 0v3.75m-.75 11.25h10.5a2.25 2.25 0 0 0 2.25-2.25v-6.75a2.25 2.25 0 0 0-2.25-2.25H6.75a2.25 2.25 0 0 0-2.25 2.25v6.75a2.25 2.25 0 0 0 2.25 2.25Z" />
+            </svg>  
+        </div>
+        <div class="profile">
+            <div class="circle-profile"></div>
+            <div class="name-profile">Nombre Apellido</div>
+            <div class="content-icon-2">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="icon-row">
+                <path stroke-linecap="round" stroke-linejoin="round" d="m19.5 8.25-7.5 7.5-7.5-7.5" />
+                </svg>
+            </div>
+        </div>
+    </div>
 </div>
