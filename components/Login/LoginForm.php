d9c9c583f6816968809633ed31794f0e4ec57149
LuisaHernandez08 <160061181+LuisaHernandez08@users.noreply.github.com>
2024-09-30 00:14:19 -0500

    Tarea formulario de login terminada

---
+++ components/Login/LoginForm.php
@@ -0,0 +1,21 @@
+<div class="login-container">
+    <div class="login-header">
+        <div class="login-title">Iniciar Sesión</div>
+    </div>
+    <div class="login-form">
+        <div class="input-group">
+            <input class="input-field" type="email" name="email" placeholder="Correo" required>
+            <input class="input-field" type="password" name="password" placeholder="Contraseña" required>
+        </div>
+        <div class="remember-me">
+            <input class="checkbox-mark" type="checkbox" id="rememberMe" name="rememberMe">
+            <label class="checkbox" for="rememberMe">Mantenerme conectado</label>
+        </div>
+        <div class="login-button">
+            <div class="button">Iniciar Sesión</div>
+        </div>
+        <div class="content-forgot-password">
+           <div class="forgot-password">Olvidaste la contraseña?</div>
+        </div>
+    </div>
+</div>
