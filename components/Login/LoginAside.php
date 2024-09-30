6755af56fb090c25de069adb8215601dcc5bdaef
henao31 <dayannahenao31@gmail.com>
2024-09-30 00:13:44 -0500

    avance 001 login aside

---
+++ components/Login/LoginAside.php
@@ -0,0 +1,20 @@
+
+<div class="container-aside-login">
+    <div class="container1"></div>
+    <div class="container2"></div>
+    <div class="container3"></div>
+    <div class="container4"></div>
+    <div class="container5"></div>
+
+    <div class="container-info-aside">
+        <div class="about">About</div>
+        <div class="privacy">Privacy</div>
+        <div class="terms">Terms of use</div>
+        <div class="faq">FAQ</div>
+   </div>
+
+    <div class="container-logo"> logo
+    
+    </div>
+
+</div>
