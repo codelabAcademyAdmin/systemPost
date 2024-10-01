263bf6da8ea51e3708edf95d6aaba538d398869a
Phamela <phamelafuentess@gmail.com>
2024-09-30 23:01:34 -0500

    Avance 2 header en dropdown

--- components/Header/Header.php
+++ components/Header/Header.php
@@ -1,10 +1,4 @@
 <div class="header">
-    <div onclick="prueba()"> prueba </div>
-
-<div id="cuervo">
-</div>
-
-
     <div class="content-profile">
         <div class="content-icon-1">
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class ="icon-lock">
