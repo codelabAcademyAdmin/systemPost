6b71cbd3ea34f425dd658a26718df8bdd3198cab
JOHANISAZAJ <johanisaza2005@gmail.com>
2024-10-02 21:10:18 -0500

    Realicé update del main

--- components/Menu/Menu.php
+++ components/Menu/Menu.php
@@ -2,21 +2,30 @@
     <div class="menu-logo">
         logo
     </div>
-    <div class="manu-options">
-
-        <div class="icon">
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
-                stroke="currentColor" class="size-6">
-                <path stroke-linecap="round" stroke-linejoin="round"
-                    d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z" />
+    <div class="menu-options">
+        <div class="option">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="icon">
+                <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 3.75h1.5l.882 3.53m0 0L6.75 14.25m-.882-6.97h13.764a.75.75 0 01.728.92l-1.2 6A.75.75 0 0118.42 15H7.08a.75.75 0 01-.73-.58l-1.2-6a.75.75 0 01.729-.92zm-.882 0L4.5 5.25m4.5 11.25a.75.75 0 100 1.5.75.75 0 000-1.5zm9 0a.75.75 0 100 1.5.75.75 0 000-1.5z"/>
             </svg>
         </div>
-
+        <div class="option">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="icon">
+                <path stroke-linecap="round" stroke-linejoin="round" d="M12 6v6l3 3m6-3a9 9 0 11-18 0 9 9 0 0118 0z"/>
+            </svg>
+        </div>
+        <div class="option">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="icon">
+                <path stroke-linecap="round" stroke-linejoin="round" d="M21 14.878V9.122a1.5 1.5 0 00-.788-1.316l-7.5-4.285a1.5 1.5 0 00-1.424 0l-7.5 4.285A1.5 1.5 0 003 9.122v5.756a1.5 1.5 0 00.788 1.316l7.5 4.285a1.5 1.5 0 001.424 0l7.5-4.285A1.5 1.5 0 0021 14.878z"/>
+                <path stroke-linecap="round" stroke-linejoin="round" d="M3.27 6.96l8.73 5.03 8.73-5.03"/>
+            </svg>
+        </div>
+        <div class="option">
+            DS
+        </div>
     </div>
-    <div class="icon"></div>
-
-    <div class="sidebar-search">
-        <div class="Search"></div>
-
+    <div class="component-search">
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="icon">
+            <path stroke-linecap="round" stroke-linejoin="round" d="M21 21l-4.35-4.35m0 0a7.5 7.5 0 10-10.607-10.607 7.5 7.5 0 0010.607 10.607z"/>
+        </svg>
     </div>
 </div>
