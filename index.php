ef600c91eac64dc3b99ccabba05c22d94ae55812
JOHANISAZAJ <johanisaza2005@gmail.com>
2024-10-07 21:14:31 -0500

    menu del layout

--- index.php
+++ index.php
@@ -6,6 +6,7 @@
    <meta http-equiv="X-UA-Compatible" content="IE=edge">
    <meta name="viewport" content="width=device-width, initial-scale=1.0">
    <link rel="stylesheet" href="assets/css/style.css">
+   <link rel="stylesheet" href="assets/font-awesome-4.7.0/css/font-awesome.min.css">
    <title>Sistema de ventas</title>
 </head>
 <body>
