dee279fb60e54283ef6e975c31f51d8089d1c13d
Sebastian Guerra <149134186+SebastianDevps@users.noreply.github.com>
2024-10-14 00:10:55 -0500

    Ajustes Finales

--- api/models/suppliers.php
+++ api/models/suppliers.php
@@ -1,145 +1,180 @@
-<?php 
+<?php
 class suppliersModel
 {
    private $conn;
 
-   public function __construct(){
+   public function __construct()
+   {
       global $conn;
       $this->conn = $conn;
    }
 
-   public function create($fullname, $phone, $address, $description, $category) {
-      
+   public function create($fullname, $phone, $address, $description, $category)
+   {
+
       $query = "INSERT INTO suppliers (fullname, phone, address, description, category) VALUES (?, ?, ?, ?, ?)";
       $stmt = $this->conn->prepare($query);
-      $stmt->bind_param("sssss", $fullname, $phone, $address, $description, $category); 
+      if (!$stmt) {
+         return [
+            'status' => 'Error',
+            'message' => 'Error al preparar la consulta: ' . $this->conn->error
+         ];
+      }
+      $stmt->bind_param("sssss", $fullname, $phone, $address, $description, $category);
 
       if ($stmt->execute()) {
-      $id_supplier = $this->conn->insert_id;
+         $id_supplier = $this->conn->insert_id;
 
-      $validation = $this->readById($id_supplier);
-      if ($validation) {
-         $stmt->close(); 
-         return [
-            'status' => 'Success',
-            'message' => 'Proveedor creado exitosamente',
-            'supplier' => $validation 
-         ];
+         $validation = $this->readById($id_supplier);
+         if ($validation) {
+            $stmt->close();
+            return [
+               'status' => 'Success',
+               'message' => 'Proveedor creado exitosamente',
+               'supplier' => $validation['supplier']
+            ];
+         } else {
+            return [
+               'status' => 'Error',
+               'message' => 'No se pudo validar la creación del proveedor'
+            ];
+         }
       } else {
          return [
             'status' => 'Error',
-            'message' => 'No se pudo validar la creación del proveedor: '
+            'message' => 'Error al crear el proveedor: ' . $stmt->error
          ];
       }
-   } else {
-      return [
-         'status' => 'Error',
-         'message' => 'Error al crear el proveedor: ' . $stmt->error
-      ];
    }
-}
 
-   public function readAll(){
+   public function readAll()
+   {
       $query = "SELECT * FROM suppliers";
       $result = $this->conn->query($query);
       return $result->fetch_all(MYSQLI_ASSOC);
    }
 
-   public function readById($id_supplier) {
+   public function readById($id_supplier)
+   {
+      if(!is_numeric($id_supplier)){
+         return[
+            'status' => 'Not Valid',
+            'message' => 'El id debe ser un numero.'
+         ];
+      }
+
       $query = "SELECT id_supplier, fullname, phone, address, description, category FROM suppliers WHERE id_supplier = ?";
       $stmt = $this->conn->prepare($query);
+      if (!$stmt) {
+         return [
+            'status' => 'Error',
+            'message' => 'Error al preparar la consulta: ' . $this->conn->error
+         ];
+      }
       $stmt->bind_param("i", $id_supplier);
       $stmt->execute();
       $result = $stmt->get_result();
       $supplier = $result->fetch_assoc();
-      $stmt->close(); 
+      $stmt->close();
 
       if (!$supplier) {
          return [
-            'status' => 'Error',
+            'status' => 'Not Found',
             'message' => 'No se encontró ningún proveedor con el ID ' . $id_supplier
          ];
-     }
-     
-     return [
+      }
+
+      return [
          'status' => 'Success',
          'supplier' => $supplier
       ];
    }
 
-   public function validationProductSuppliers($id_supplier) {
+   public function validationProductSuppliers($id_supplier)
+   {
       $query = "SELECT COUNT(*) as total FROM products_suppliers WHERE id_supplier = ?";
       $stmt = $this->conn->prepare($query);
       $stmt->bind_param("i", $id_supplier);
       $stmt->execute();
       $result = $stmt->get_result();
       $supplier = $result->fetch_assoc();
-      $stmt->close(); 
+      $stmt->close();
 
       return $supplier;
    }
 
 
-   public function update($id_supplier, $fullname, $phone, $address, $description, $category) {
-      $query = "SELECT id_supplier FROM suppliers WHERE id_supplier = ?";
-      $stmt = $this->conn->prepare($query);
-      $stmt->bind_param("i", $id_supplier);
-      $stmt->execute();
-      $stmt->store_result();
-  
-      if ($stmt->num_rows === 0) {
-          return [
-              'status' => 'Error',
-              'message' => 'ID no existe' 
-          ];
+   public function update($id_supplier, $fullname, $phone, $address, $description, $category)
+   {
+      if(!is_numeric($id_supplier)){
+         return[
+            'status' => 'Not Valid',
+            'message' => 'El id debe ser un numero.'
+         ];
       }
-  
+
+      $validation = $this->readById($id_supplier);
+      if(empty($validation['supplier'])){
+         return [
+            'status' => 'Not Found',
+            'message' => 'No se encontro el proveedor con ese id'
+         ];
+      };
+
       $query = "UPDATE suppliers SET fullname = ?, phone = ?, address = ?, description = ?, category = ? WHERE id_supplier = ?";
       $stmt = $this->conn->prepare($query);
-      
-      if (!$stmt) {
-          return [
-              'status' => 'Error',
-              'message' => 'Error al preparar la consulta: ' . $this->conn->error
-          ];
-      }
-  
-      $stmt->bind_param("ssssss", $fullname, $phone, $address, $description, $category, $id_supplier);
-  
-      if ($stmt->execute()) {
-          if ($stmt->error) {
-              return [
-                  'status' => 'Error',
-                  'message' => 'Error al ejecutar la consulta: ' . $stmt->error
-              ];
-          }
-  
-          if ($stmt->affected_rows > 0) {
-              return [
-                  'status' => 'Exito',
-                  'message' => 'Proveedor actualizado exitosamente.',
-                  'supplier' => $this->readById($id_supplier)
-              ];
-          } else {
-              return [
-                  'status' => 'Advertencia',
-                  'message' => 'No se hicieron cambios, los datos son iguales al proveedor existente.'
-              ];
-          }
-      } else {
-          return [
-              'status' => 'Error',
-              'message' => 'Error al actualizar el proveedor: ' . $stmt->error
-          ];
-      }
-  }
-  
 
-   public function delete($id_supplier){
-      $supplier = $this->readById($id_supplier);
-      if ($supplier['status'] == 'Error') {
+      if (!$stmt) {
          return [
             'status' => 'Error',
+            'message' => 'Error al preparar la consulta: ' . $this->conn->error
+         ];
+      }
+
+      $stmt->bind_param("ssssss", $fullname, $phone, $address, $description, $category, $id_supplier);
+
+      if ($stmt->execute()) {
+         if ($stmt->error) {
+            return [
+               'status' => 'Error',
+               'message' => 'Error al ejecutar la consulta: ' . $stmt->error
+            ];
+         }
+
+         if ($stmt->affected_rows > 0) {
+            $update = $this->readById($id_supplier);
+            return [
+               'status' => 'Success',
+               'message' => 'Proveedor actualizado exitosamente.',
+               'supplier' => $update['supplier']
+            ];
+         } else{
+            return [
+               'status' => 'Success',
+               'message' => 'No se actulizaron datos.'
+            ];
+         }
+      } else {
+         return [
+            'status' => 'Error',
+            'message' => 'Error al actualizar el proveedor: ' . $stmt->error
+         ];
+      }
+   }
+
+
+   public function delete($id_supplier)
+   {
+      if(!is_numeric($id_supplier)){
+         return[
+            'status' => 'Not Valid',
+            'message' => 'El id debe ser un numero.'
+         ];
+      }
+      $supplier = $this->readById($id_supplier);
+      if ($supplier['status'] == 'Not Found') {
+         return [
+            'status' => 'Not Found',
             'message' => 'No se puede eliminar el proveedor porque el ID ' . $id_supplier . ' no existe.'
          ];
       }
@@ -147,11 +182,11 @@ class suppliersModel
       $validation = $this->validationProductSuppliers($id_supplier);
       if ($validation['total'] > 0) {
          return [
-            'status' => 'Error',
+            'status' => 'Conflict',
             'message' => 'No se puede eliminar el proveedor porque tiene productos relacionados.'
          ];
-      } 
-      
+      }
+
       $query = "DELETE FROM suppliers WHERE id_supplier = ?";
       $stmt = $this->conn->prepare($query);
       $stmt->bind_param("i", $id_supplier);
@@ -170,5 +205,3 @@ class suppliersModel
       ];
    }
 }
-
-?>
--- api/routes/route.suppliers.php
+++ api/routes/route.suppliers.php
@@ -1,97 +1,112 @@
-<?php 
-   $AppRoutes->AddRoutes('GET', 'suppliers', function() {
-      require_once 'models/suppliers.php';
-      $supplies = new suppliersModel();
-      $response;
-  
-      if (isset($_GET['id'])) {
-         $id = $_GET['id'];
-         $response = $supplies->readById($id);
-  
-         if ($response['status'] === 'Error') {
-            http_response_code(404);
-         } else {
-            http_response_code(200);
-         }
-      } else {
-         $response = $supplies->readAll();
-         http_response_code(200);
-      }
-      
-      echo json_encode($response);
-   });
+<?php
+$AppRoutes->AddRoutes('GET', 'suppliers', function () {
+   require_once 'models/suppliers.php';
+   $supplies = new suppliersModel();
+   $response;
 
-   $AppRoutes->AddRoutes('POST', 'suppliers', function() {
-      require_once 'models/suppliers.php';
-      $suppliers = new suppliersModel();
-      $response;
-      $data = json_decode(file_get_contents('php://input'), true);
+   if (isset($_GET['id'])) {
+      $id = $_GET['id'];
+      $response = $supplies->readById($id);
 
-      if ($data) {
-         $response = $suppliers->create($data['fullname'], $data['phone'], $data['address'], $data['description'], $data['category']);
-         
-         if ($response) {
-            http_response_code(201); 
-            echo json_encode(['message' => 'Proovedor creado exitosamente']);
-         } else {
-            http_response_code(400); 
-            echo json_encode(['message' => 'Datos de entrada no son validos']);
-         }
-      } else {
-         http_response_code(500); 
-         echo json_encode(['message' => 'No se pudo crear el proveedor']);
+      switch ($response['status']) {
+         case 'Not Valid':
+            http_response_code(400); // Bad Request
+            break;
+         case 'Success':
+            http_response_code(200); // Ok
+            break;
+         case 'Not Found':
+            http_response_code(404); // Not Found
+            break;
+         case 'Error':
+            http_response_code(500); // Internal Server Error
+            break;
       }
-   });
+   } else {
+      $response = $supplies->readAll();
+      http_response_code(200);
+   }
 
-   $AppRoutes->AddRoutes('PUT', 'suppliers', function() {
-      require_once 'models/suppliers.php';
-      $supplies = new suppliersModel();
-  
-      if (empty($_GET['id'])) {
-         http_response_code(400); 
-         echo json_encode(['status' => 'Error', 'message' => 'ID del proveedor es requerido.']);
-         return;
-      }
-  
-      $id_supplier = (int)$_GET['id']; 
-      $data = json_decode(file_get_contents('php://input'), true);
-  
-      if (!isset($data['fullname'], $data['phone'], $data['address'], $data['description'], $data['category'])) {
-         http_response_code(400); 
-         echo json_encode(['status' => 'Error', 'message' => 'Faltan datos requeridos.']);
-         return;
-      }
-  
-      $response = $supplies->update($id_supplier, $data['fullname'], $data['phone'], $data['address'], $data['description'], $data['category']);
-  
-      if ($response['status'] === 'Exito') {
-         http_response_code(200); 
-      } else {
+   echo json_encode($response);
+});
+
+$AppRoutes->AddRoutes('POST', 'suppliers', function () {
+   require_once 'models/suppliers.php';
+   $suppliers = new suppliersModel();
+   $response;
+   $data = json_decode(file_get_contents('php://input'), true);
+
+   if ($data) {
+      $response = $suppliers->create($data['fullname'], $data['phone'], $data['address'], $data['description'], $data['category']);
+
+      if ($response['status'] === 'Success') {
+         http_response_code(201);
+      } else if ($response['status'] === 'Error') {
          http_response_code(500);
       }
+   }
+
+   echo json_encode($response);
+});
+
+$AppRoutes->AddRoutes('PUT', 'suppliers', function () {
+   require_once 'models/suppliers.php';
+   $supplies = new suppliersModel();
+
+   $id_supplier = $_GET['id'];
+   $data = json_decode(file_get_contents('php://input'), true);
+
+   if (!isset($data['fullname'], $data['phone'], $data['address'], $data['description'], $data['category'])) {
+      http_response_code(400);
+      echo json_encode(['status' => 'Error', 'message' => 'Faltan datos requeridos.']);
+   }
+
+   $response = $supplies->update($id_supplier, $data['fullname'], $data['phone'], $data['address'], $data['description'], $data['category']);
+
+   switch ($response['status']) {
+      case 'Not Valid':
+         http_response_code(400); // Bad Request
+         break;
+      case 'Success':
+         http_response_code(200); // Ok
+         break;
+      case 'Not Found':
+         http_response_code(404); // Not Found
+         break;
+      case 'Error':
+         http_response_code(500); // Internal Server Error
+         break;
       
-      echo json_encode($response);
-  });
-  
-  $AppRoutes->AddRoutes('DELETE', 'suppliers', function() {
+   }
+
+   echo json_encode($response);
+});
+
+$AppRoutes->AddRoutes('DELETE', 'suppliers', function () {
    require_once 'models/suppliers.php';
    $suppliers = new suppliersModel();
    $id = $_GET['id'];
 
-   if (isset($id)) {
-      $response = $suppliers->delete($id);
+   $response = $suppliers->delete($id);
 
-      if ($response['status'] === 'Success') {
-         http_response_code(200); 
-         } else {
-           http_response_code(400);
-         }
-      echo json_encode($response);
-   } else {
-      http_response_code(400); 
-      echo json_encode(['status' => 'Error', 'message' => 'ID del proveedor es requerido.']);
+   switch ($response['status']) {
+      case 'Not Valid':
+         http_response_code(400); // Bad Request
+         break;
+      case 'Success':
+         http_response_code(200); // Ok
+         break;
+      case 'Not Found':
+         http_response_code(404); // Not Found
+         break;
+      case 'Error':
+         http_response_code(500); // Internal Server Error
+         break;
+      case 'Conflict':
+         http_response_code(409); //Conflict
+         break;
    }
 
-});
+   echo json_encode($response);
 
-?>
+});
