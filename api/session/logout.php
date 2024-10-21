f6b432f8aed2ba25936e5a841f628151c5069601
Deyler Mena <deyler_mena69222@elpoli.edu.co>
2024-10-21 18:26:55 -0500

    redirigimos al index cuando cierra sesion

--- api/session/logout.php
+++ api/session/logout.php
@@ -4,4 +4,6 @@ session_start(); // Reanudar la sesión existente
 session_unset(); // Eliminar todas las variables de sesión
 session_destroy(); // Destruir la sesión
 
+header('Location: ../../index.php');
+exit;
 ?>
