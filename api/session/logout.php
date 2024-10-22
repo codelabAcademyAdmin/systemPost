9dd7eef796936969650bcaa2d35e5c8b9baf9835
Deyler Mena <deyler_mena69222@elpoli.edu.co>
2024-10-21 21:05:09 -0500

    borramos enrutamiento en logout

--- api/session/logout.php
+++ api/session/logout.php
@@ -4,6 +4,4 @@ session_start(); // Reanudar la sesión existente
 session_unset(); // Eliminar todas las variables de sesión
 session_destroy(); // Destruir la sesión
 
-header('Location: ../../index.php');
-exit;
 ?>
