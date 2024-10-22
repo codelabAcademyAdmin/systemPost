2c0a865ff47a86260212f7b4b0b225ac9d248fd6
JuanesEsp1 <juanes.espinosa01@gmail.com>
2024-10-21 20:42:50 -0500

    componente de tabla y paginador

--- components/Table/Table.php
+++ components/Table/Table.php
@@ -180,7 +180,26 @@ $productos = [
       'cantidad' => 100,
       'activo' => true,
       'fecha_creacion' => '26/01/2024'
+   ],
+   [
+      'id' => 21,
+      'nombre' => 'Adaptador USB-C',
+      'descripcion' => 'Hub multipuerto con HDMI y lector de tarjetas',
+      'precio' => 69.99,
+      'cantidad' => 100,
+      'activo' => true,
+      'fecha_creacion' => '26/01/2024'
+   ],
+   [
+      'id' => 22,
+      'nombre' => 'Adaptador USB-C',
+      'descripcion' => 'Hub multipuerto con HDMI y lector de tarjetas',
+      'precio' => 69.99,
+      'cantidad' => 100,
+      'activo' => true,
+      'fecha_creacion' => '26/01/2024'
    ]
+
 ];
 
 
@@ -209,6 +228,18 @@ function getPaginationRange($paginaActual, $totalPaginas, $maxPaginas = 3)
 
 $paginasAMostrar = getPaginationRange($paginaActual, $totalPaginas, 3);
 
+
+function formatText($text)
+{
+   if ($text == null) {
+      return $text;
+   }
+   if (strlen($text) > 50) {
+      return substr($text, 0, 30) . '...';
+   }
+   return $text;
+};
+
 ?>
 
 <div class="container">
@@ -231,7 +262,7 @@ $paginasAMostrar = getPaginationRange($paginaActual, $totalPaginas, 3);
                <tr>
                   <td><?php echo $producto['id']; ?></td>
                   <td><?php echo $producto['nombre']; ?></td>
-                  <td><?php echo $producto['descripcion']; ?></td>
+                  <td><?php echo  formatText($producto['descripcion']) ?></td>
                   <td><?php echo $producto['precio']; ?></td>
                   <td class="text-center"><?php echo $producto['cantidad']; ?></td>
                   <td class="text-center">
@@ -241,18 +272,18 @@ $paginasAMostrar = getPaginationRange($paginaActual, $totalPaginas, 3);
                   </td>
                   <td class="text-center"><?php echo $producto['fecha_creacion']; ?></td>
                   <td class="text-center acciones">
-                     <button class="btn-edit">
+                     <button class="btn-accions">
                         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="icon">
                            <path d="M21.731 2.269a2.625 2.625 0 0 0-3.712 0l-1.157 1.157 3.712 3.712 1.157-1.157a2.625 2.625 0 0 0 0-3.712ZM19.513 8.199l-3.712-3.712-8.4 8.4a5.25 5.25 0 0 0-1.32 2.214l-.8 2.685a.75.75 0 0 0 .933.933l2.685-.8a5.25 5.25 0 0 0 2.214-1.32l8.4-8.4Z" />
                            <path d="M5.25 5.25a3 3 0 0 0-3 3v10.5a3 3 0 0 0 3 3h10.5a3 3 0 0 0 3-3V13.5a.75.75 0 0 0-1.5 0v5.25a1.5 1.5 0 0 1-1.5 1.5H5.25a1.5 1.5 0 0 1-1.5-1.5V8.25a1.5 1.5 0 0 1 1.5-1.5h5.25a.75.75 0 0 0 0-1.5H5.25Z" />
                         </svg>
                      </button>
-                     <button class="btn-delete">
+                     <button class="btn-accions">
                         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="icon">
                            <path fill-rule="evenodd" d="M16.5 4.478v.227a48.816 48.816 0 0 1 3.878.512.75.75 0 1 1-.256 1.478l-.209-.035-1.005 13.07a3 3 0 0 1-2.991 2.77H8.084a3 3 0 0 1-2.991-2.77L4.087 6.66l-.209.035a.75.75 0 0 1-.256-1.478A48.567 48.567 0 0 1 7.5 4.705v-.227c0-1.564 1.213-2.9 2.816-2.951a52.662 52.662 0 0 1 3.369 0c1.603.051 2.815 1.387 2.815 2.951Zm-6.136-1.452a51.196 51.196 0 0 1 3.273 0C14.39 3.05 15 3.684 15 4.478v.113a49.488 49.488 0 0 0-6 0v-.113c0-.794.609-1.428 1.364-1.452Zm-.355 5.945a.75.75 0 1 0-1.5.058l.347 9a.75.75 0 1 0 1.499-.058l-.346-9Zm5.48.058a.75.75 0 1 0-1.498-.058l-.347 9a.75.75 0 0 0 1.5.058l.345-9Z" clip-rule="evenodd" />
                         </svg>
                      </button>
-                     <button class="btn-view">
+                     <button class="btn-accions">
                         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="icon">
                            <path d="M12 15a3 3 0 1 0 0-6 3 3 0 0 0 0 6Z" />
                            <path fill-rule="evenodd" d="M1.323 11.447C2.811 6.976 7.028 3.75 12.001 3.75c4.97 0 9.185 3.223 10.675 7.69.12.362.12.752 0 1.113-1.487 4.471-5.705 7.697-10.677 7.697-4.97 0-9.186-3.223-10.675-7.69a1.762 1.762 0 0 1 0-1.113ZM17.25 12a5.25 5.25 0 1 1-10.5 0 5.25 5.25 0 0 1 10.5 0Z" clip-rule="evenodd" />
@@ -265,171 +296,24 @@ $paginasAMostrar = getPaginationRange($paginaActual, $totalPaginas, 3);
       </table>
    </div>
 
-   <div class="">
+   <div class="container-pagination">
       <div class="pagination">
          <a href="?pagina=<?php echo max(1, $paginaActual - 1); ?>" class="pagination-arrow <?php echo $paginaActual == 1 ? 'disabled' : ''; ?>">
-            &lt;
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="icon-paginator">
+               <path fill-rule="evenodd" d="M12 2.25c-5.385 0-9.75 4.365-9.75 9.75s4.365 9.75 9.75 9.75 9.75-4.365 9.75-9.75S17.385 2.25 12 2.25Zm-4.28 9.22a.75.75 0 0 0 0 1.06l3 3a.75.75 0 1 0 1.06-1.06l-1.72-1.72h5.69a.75.75 0 0 0 0-1.5h-5.69l1.72-1.72a.75.75 0 0 0-1.06-1.06l-3 3Z" clip-rule="evenodd" />
+            </svg>
          </a>
          <?php foreach ($paginasAMostrar as $pagina): ?>
-            <a href="?pagina=<?php echo $pagina; ?>" class="pagination-number <?php echo $pagina === $paginaActual ? 'active' : ''; ?>">
+            <a href="?pagina=<?php echo $pagina; ?>" class="pagination-number <?php echo $pagina == $paginaActual ? 'active' : ''; ?>">
                <?php echo $pagina; ?>
             </a>
          <?php endforeach; ?>
          <a href="?pagina=<?php echo min($totalPaginas, $paginaActual + 1); ?>" class="pagination-arrow <?php echo $paginaActual == $totalPaginas ? 'disabled' : ''; ?>">
-            &gt;
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="icon-paginator">
+               <path fill-rule="evenodd" d="M12 2.25c-5.385 0-9.75 4.365-9.75 9.75s4.365 9.75 9.75 9.75 9.75-4.365 9.75-9.75S17.385 2.25 12 2.25Zm4.28 10.28a.75.75 0 0 0 0-1.06l-3-3a.75.75 0 1 0-1.06 1.06l1.72 1.72H8.25a.75.75 0 0 0 0 1.5h5.69l-1.72 1.72a.75.75 0 1 0 1.06 1.06l3-3Z" clip-rule="evenodd" />
+            </svg>
          </a>
       </div>
    </div>
 
 </div>
-
-<style>
-   .pagination {
-      display: flex;
-      justify-content: center;
-      align-items: center;
-      margin-top: 20px;
-      font-family: Arial, sans-serif;
-   }
-
-   .pagination a {
-      text-decoration: none;
-      color: #4a5568;
-      padding: 8px 12px;
-      margin: 0 2px;
-      border-radius: 4px;
-      transition: background-color 0.3s, color 0.3s;
-   }
-
-   .pagination-number {
-      background-color: #edf2f7;
-   }
-
-   .pagination-number.active {
-      background-color: #4a5568;
-      color: white;
-   }
-
-   .pagination-arrow {
-      background-color: #edf2f7;
-      font-weight: bold;
-   }
-
-   .pagination-arrow.disabled {
-      opacity: 0.5;
-      cursor: not-allowed;
-   }
-
-   .pagination a:hover:not(.active):not(.disabled) {
-      background-color: #e2e8f0;
-   }
-
-
-
-
-
-   .container {
-      display: flex;
-      justify-content: center;
-      align-items: center;
-      height: calc(100vh - 100px);
-      flex-direction: column;
-   }
-
-   .table-container {
-      /* min-width: 100%; */
-      max-width: 1200px;
-      overflow-y: auto;
-      overflow-x: auto;
-      max-height: 500px;
-      box-shadow: 0 4px 6px -1px rgba(0, 0, 0, 0.1), 0 2px 4px -1px rgba(0, 0, 0, 0.06);
-      margin-top: 8px;
-      border-radius: 8px;
-   }
-
-   .product-table {
-      min-width: 100%;
-      table-layout: auto;
-      background-color: white;
-      border-radius: 8px;
-   }
-
-   .product-table thead tr {
-      background-color: #e5e7eb;
-      text-align: left;
-      color: #4b5563;
-      text-transform: uppercase;
-      font-size: 14px;
-      line-height: 20px;
-   }
-
-   .product-table th {
-      padding: 8px 24px;
-   }
-
-   .product-table td {
-      padding: 12px 24px;
-   }
-
-   .product-table tbody tr {
-      border-bottom: 1px solid #e5e7eb;
-   }
-
-   .product-table tbody tr:hover {
-      background-color: #f3f4f6;
-   }
-
-   .text-center {
-      text-align: center;
-   }
-
-   .status-active {
-      background-color: #99A5E0;
-      color: white;
-      padding: 8px 16px;
-      border-radius: 9999px;
-      font-size: 14px;
-   }
-
-   .status-inactive {
-      background-color: #fecaca;
-      color: #b91c1c;
-      padding: 8px 16px;
-      border-radius: 9999px;
-      font-size: 14px;
-   }
-
-   .acciones {
-      display: flex;
-      flex-direction: row;
-      gap: 6px;
-   }
-
-   .icon {
-      width: 24px;
-      height: 24px;
-   }
-
-   .btn-edit,
-   .btn-delete,
-   .btn-view {
-      width: 40px;
-      height: 40px;
-      border-radius: 9999px;
-      border: none;
-      cursor: pointer;
-      background-color: #E6EBF9;
-      display: flex;
-      justify-content: center;
-      align-items: center;
-      color: #6668C5;
-      transition: all 0.3s ease;
-   }
-
-   .btn-delete:hover,
-   .btn-edit:hover,
-   .btn-view:hover {
-      background-color: #6668C5;
-      color: #E6EBF9;
-   }
-</style>
