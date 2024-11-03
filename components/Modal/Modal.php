329f8ee8d78421f67dc0cdc3669721f57faf8aaf
Phamela <phamelafuentess@gmail.com>
2024-11-02 23:36:10 -0500

    Creacion - avance del componente del modal

---
+++ components/Modal/Modal.php
@@ -0,0 +1,16 @@
+<div class="content-modal">
+    <div class="content-form">
+        <div class="header-modal">
+            <div class="title">Agregar Producto</div>
+            <div class="separator"></div>
+        </div>
+        <div class="content-info">
+            Contenido 
+            <!-- Aqui va el formulario o contenido -->
+        </div>
+        <div class="content-buttons">
+            <div class="btn button-cancel">Cancelar</div>
+            <div class="btn button-confirm">Agregar</div>
+        </div>
+    </div>
+</div>
