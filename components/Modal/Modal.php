de6ad875f4fe4734b3f0f94024bf20f41dd91fb9
Phamela <phamelafuentess@gmail.com>
2024-11-04 21:56:09 -0500

    Se termino la creacion del modulo

--- components/Modal/Modal.php
+++ components/Modal/Modal.php
@@ -1,7 +1,15 @@
-<div class="content-modal">
-    <div class="content-form">
-        <div class="header-modal">
-            <div class="title">Agregar Producto</div>
+<div class="content-modal" style="display: none;" >
+    <div class="content-modal-container">
+        <div class="content-form">
+            <div class="header-modal">
+            <div class="content-title">
+                <div class="title">Agregar Producto</div>
+                <div class="close-modal" onclick='toggleModal()' >
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="cancel-icon-modal">
+                        <path stroke-linecap="round" stroke-linejoin="round" d="M6 18 18 6M6 6l12 12" />
+                    </svg>
+                </div>
+            </div>
             <div class="separator"></div>
         </div>
         <div class="content-info">
@@ -9,8 +17,9 @@
             <!-- Aqui va el formulario o contenido -->
         </div>
         <div class="content-buttons">
-            <div class="btn button-cancel">Cancelar</div>
-            <div class="btn button-confirm">Agregar</div>
+            <div class="btn-modal button-cancel" onclick='toggleModal()'>Cancelar</div>
+            <div class="btn-modal button-confirm">Agregar</div>
+            </div>
         </div>
     </div>
 </div>
